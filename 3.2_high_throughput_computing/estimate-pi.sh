0c5041bdd941921760f29d2a3004293c92122419
Marty Kandes <mkandes@sdsc.edu>
2025-08-04 18:30:53 -0700

    Update estimate-pi.sh

--- 3.2_high_throughput_computing/estimate-pi.sh
+++ 3.2_high_throughput_computing/estimate-pi.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=estimate-pi
 #SBATCH --account=gue998
-#SBATCH --reservation=si24
+#SBATCH --reservation=si25cpu
 #SBATCH --partition=shared
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
