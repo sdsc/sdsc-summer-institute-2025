220069b466d7cdc5b9b1a948c6a5f4e02326b261
Andrea Zonca <code@andreazonca.com>
2025-08-07 22:31:54 -0700

    fix scheduler port

--- 6.1_python_for_HPC/dask_slurm/launch_worker.sh
+++ 6.1_python_for_HPC/dask_slurm/launch_worker.sh
@@ -2,4 +2,4 @@ echo "Launching dask worker"
 MEM_GB=240
 # memory limit is in bytes
 MEM=$(( $MEM_GB*1024**3 ))
-dask worker --scheduler-file ~/.dask_scheduler.json --memory-limit $MEM --nworkers 1
+dask worker --scheduler-file ~/.dask_scheduler.json --memory-limit $MEM --nworkers 1 --nthreads 128
